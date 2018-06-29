ec08da670da0bfe3a63535d40f405b5b99865996
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-06-29 12:51:20 -0500

    chore: create gulp tasks for image optimization and resizing

--- gulpfile.js
+++ gulpfile.js
@@ -1,131 +1,32 @@
-// // from gulpjs.org
-// const gulp = require('gulp');
-
-// // responsive image set up from
-// // https://www.webstoemp.com/blog/responsive-images-pipeline-with-gulp/
-// // const del = require('del');
-// const deleteEmpty = require('delete-empty');
-// // const globby = require('globby');
-// // const gulpImagemin = require('gulp-imagemin');
-// const gulpImageresize = require('gulp-image-resize');
-// // const gulpNewer = require('gulp-newer');
-// // const merge2 = require('merge2');
-
-// // TODO: update fields to be specific to THIS project
-// const transforms = [
-//   {
-//     src: './img/*',
-//     dist: './img/img-opt/',
-//     params: {
-//       width: 800,
-//       quality: 0.7,
-//       // format: jpeg,
-//       sharpen: true,
-//       interlace: true
-//       // background: none
-//     }
-//     // },
-//     // {
-//     //   src: './img/*',
-//     //   dist: './img/img-opt/',
-//     //   params: {
-//     //     width: 600,
-//     //     quality: 0.7,
-//     //     // format: jpeg,
-//     //     sharpen: true,
-//     //     interlace: true
-//     //     // background: none
-//   }
-//   // },
-//   // {
-//   //   src: './img/*',
-//   //   dist: './img/img-opt/',
-//   //   params: {
-//   //     width: 400,
-//   //     quality: 0.7,
-//   //     // format: jpeg,
-//   //     sharpen: true,
-//   //     interlace: true
-//   //     // background: none
-//   //   }
-//   // }
-// ];
-
-// // from gulpjs.org
-// gulp.task('img', function() {
-//   return transforms;
-// });
-
-// // gulp.task('default', function() {
-// //   console.log('Hello World!');
-// // });
-
-// // Default Task
-// // gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
-// // from https://travismaynard.com/writing/getting-started-with-gulp
-// // gulp.task('default', ['img']);
-
-// from https://jonathanmh.com/image-resizing-gulp/
-// var gulp = require('gulp');
-// var gm = require('gulp-gm');
-// var newer = require('gulp-newer');
-// var imagemin = require('gulp-imagemin');
-
-// gulp.task('default', function() {
-//   gulp
-//     .src('img/**/*.*')
-//     .pipe(newer('resized'))
-//     .pipe(
-//       gm(function(gmfile) {
-//         gmfile.setFormat('jpg').quality(90);
-//         return gmfile.resize(600, 500);
-//       })
-//     )
-//     .pipe(imagemin())
-//     .pipe(gulp.dest('resized'));
-// });
-
+// from gulpjs.org
 const gulp = require('gulp');
+
+// used to figure out syntax for multiple sizes: https://stackoverflow.com/questions/30583051/gulp-image-resize-with-multiple-images, couldn't quite get it to work
+// heavily modified responsive image set up from
+// https://www.webstoemp.com/blog/responsive-images-pipeline-with-gulp/
+// another post I used and butchered code from
+// from https://jonathanmh.com/image-resizing-gulp/
+// not much remains from the experimewnts using the above posts...
+// but there are micro-snippets and syntax that remain like 'img/*.*'
+
+// QUESTION: I tried to get all of the img tasks to be in the same task,
+// but kept getting weird uotputs (1 img name 1-400-600-800.jpg). I couldn't find any helpfulm info on how to chain them into a single task. Is that possible? Is it recommeneded?
+
 const imagemin = require('gulp-imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageResize = require('gulp-image-resize');
 const rename = require('gulp-rename');
-// const gm = require('gm').subClass({ imageMagick: true });
-var fs = require('fs'),
-  gm = require('gm').subClass({ imageMagick: true });
+const gm = require('gm');
 
-// ftom google dev docs on img optimization
-// gulp.task('img', function() {
-//   return gulp
-//     .src('img/*')
-//     .pipe(
-//       imagemin({
-//         interlaced: true,
-//         progressive: true,
-//         optimizationLevel: 5
-//       })
-//     )
-//     .pipe(
-//       imagemin([
-//         imageminMozjpeg({
-//           quality: 85
-//         })
-//       ])
-//     )
-//     .pipe(gulp.dest('dist-img'));
-// });
-
-gulp.task('img', function() {
+gulp.task('img-sm', function() {
   return (gulp
-      .src('img/*')
+      .src('img/*.*')
       // smallest file size
-      // used to figure out syntax for multiple sizes: https://stackoverflow.com/questions/30583051/gulp-image-resize-with-multiple-images
       .pipe(imageResize({ width: 400 }))
       .pipe(
         imagemin({
           interlaced: true,
-          progressive: true,
-          optimizationLevel: 5
+          progressive: true
         })
       )
       // found Mozjpeg stuff on google developer docs on automating image optimization
@@ -136,19 +37,22 @@ gulp.task('img', function() {
           })
         ])
       )
-      // .pipe(gulp.dest('img/img-opt'))
       .pipe(
         rename({
           suffix: '-400'
         })
       )
-      // mid size
+      .pipe(gulp.dest('img/img-opt')) );
+});
+
+gulp.task('img-md', function() {
+  return (gulp
+      .src('img/*.*')
       .pipe(imageResize({ width: 600 }))
       .pipe(
         imagemin({
           interlaced: true,
-          progressive: true,
-          optimizationLevel: 5
+          progressive: true
         })
       )
       // found Mozjpeg stuff on google developer docs on automating image optimization
@@ -159,19 +63,22 @@ gulp.task('img', function() {
           })
         ])
       )
-      // .pipe(gulp.dest('img/img-opt'))
       .pipe(
         rename({
           suffix: '-600'
         })
       )
-      // large size (most likely same dimensions as orig)
+      .pipe(gulp.dest('img/img-opt')) );
+});
+
+gulp.task('img-lg', function() {
+  return (gulp
+      .src('img/*.*')
       .pipe(imageResize({ width: 800 }))
       .pipe(
         imagemin({
           interlaced: true,
-          progressive: true,
-          optimizationLevel: 5
+          progressive: true
         })
       )
       // found Mozjpeg stuff on google developer docs on automating image optimization
@@ -182,7 +89,6 @@ gulp.task('img', function() {
           })
         ])
       )
-      // .pipe(gulp.dest('img/img-opt'))
       .pipe(
         rename({
           suffix: '-800'
@@ -190,3 +96,36 @@ gulp.task('img', function() {
       )
       .pipe(gulp.dest('img/img-opt')) );
 });
+
+gulp.task('img-full', function() {
+  return (gulp
+      .src('img/*.*')
+      .pipe(
+        imagemin({
+          interlaced: true,
+          progressive: true
+        })
+      )
+      // found Mozjpeg stuff on google developer docs on automating image optimization
+      .pipe(
+        imagemin([
+          imageminMozjpeg({
+            quality: 95
+          })
+        ])
+      )
+      .pipe(
+        rename({
+          suffix: '-full'
+        })
+      )
+      .pipe(gulp.dest('img/img-opt')) );
+});
+
+// // Default Task
+// // gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+// // from https://travismaynard.com/writing/getting-started-with-gulp
+
+// here's where I got the .parallel and learned to update my gulp -v
+// https://blog.wearewizards.io/migrating-to-gulp-4-by-example
+gulp.task('default', gulp.parallel('img-sm', 'img-md', 'img-lg', 'img-full'));
